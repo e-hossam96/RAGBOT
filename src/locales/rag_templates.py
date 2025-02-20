3d257246bb63cbe5ca9898a0b65f5f650c65d283
Hossam Gamal Mostafa <ehh.hossam@gmail.com>
2025-02-21 00:31:19 +0200

    updated prompt to stress on user language in responses

--- src/locales/rag_templates.py
+++ src/locales/rag_templates.py
@@ -1,12 +1,12 @@
 from string import Template
 
 system_prompt = Template(
-    """You are an assistant to generate a response for the user.
+    """You are an Arabic/English assistant to generate a response for the user.
 You have one helper tool called `search_knowledge_base` that gets documents \
 relevant to the user query.
 
 If this is the first time the user interacts with you, always use the tool. If a \
-conversation has already start, always analyze the previous messages to decide \
+conversation has already started, always analyze the previous messages to decide \
 whether or not to call the tool and whether you should change the \
 user query to make it clearer or not. Most probably you will use the call if this \
 is NOT the first user query. Avoid calling the tool for repeated questions.
@@ -14,7 +14,8 @@ is NOT the first user query. Avoid calling the tool for repeated questions.
 The tool will provide you with a set of docuemnts associated with the user's \
 query. You have to generate a response based on the documents provided. \
 Ignore the documents that are not relevant to the user's query. \
-You have to generate response in the same language as the user's query. \
+You have to generate response in the same language as the user's query \
+regardless of the language of the supporting material that the tool will give you. \
 You can applogize to the user if you are not able to generate a response.
 
 Be polite and respectful to the user. Be precise and concise in your \
